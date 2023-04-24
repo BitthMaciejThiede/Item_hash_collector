befd524d45ed253f303e08773228d91a9ca6c0e8
unknown <bitth.maciej.thiede@gmail.com>
2023-04-24 21:36:57 +0200

    add service to cron

--- src/main/java/pl/steam/market/item_hash_collector/cron/ItemHashCron.java
+++ src/main/java/pl/steam/market/item_hash_collector/cron/ItemHashCron.java
@@ -1,8 +1,10 @@
 package pl.steam.market.item_hash_collector.cron;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import pl.steam.market.item_hash_collector.service.ItemHashService;
 
 //TODO Add more descriptive name
 
@@ -10,9 +12,15 @@ import org.springframework.stereotype.Component;
 @EnableScheduling
 public class ItemHashCron {
 
+    private final ItemHashService service;
+
+    @Autowired
+    public ItemHashCron(ItemHashService service) {
+        this.service = service;
+    }
 
     @Scheduled(cron = "0 22 * * *")
     void scheduled() {
-
+        service.runCollection();
     }
 }
